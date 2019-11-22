0ba16a39c08cd48268cd7622f963a515370c952b
Kamal Maged <kamal_maged@yahoo.com>
2019-11-22 07:39:26 -0500

    Initial Commit

---
+++ GiphyKin/GiphyTrendingCell.swift
@@ -0,0 +1,24 @@
+//
+//  GiphyTableViewCell.swift
+//  GiphyKin
+//
+//  Created by Kamal Maged on 2019-11-22.
+//  Copyright © 2019 Kamal Maged. All rights reserved.
+//
+
+import UIKit
+
+class GiphyTrendingCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

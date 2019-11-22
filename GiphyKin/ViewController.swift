11e2c3fd10b0308e215b0ecc61f08e86692c014b
Kamal Maged <kamal_maged@yahoo.com>
2019-11-22 07:21:28 -0500

    Initial Commit

---
+++ GiphyKin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GiphyKin
+//
+//  Created by Kamal Maged on 2019-11-22.
+//  Copyright © 2019 Kamal Maged. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
